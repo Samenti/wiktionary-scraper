8dee1c2425564e6c0c2d493ef8bb3fbbbd6e0ec5
Raznor <62889081+Raznor@users.noreply.github.com>
2020-06-09 19:07:00 +0200

    now imports nominals from nominals.txt with io module

--- wiktionary-scraper.py
+++ wiktionary-scraper.py
@@ -9,5 +9,14 @@ declension table data from Wiktionary.org.
 
 # Max line length w/ --- w/o flowing text blocks (PEP 8 style guide): 72 --- 79
 
+import io
 import requests
 from lxml import html
+
+URL_PREFIX = u'https://en.wiktionary.org/wiki/'
+
+words = []
+with io.open("nominals.txt", mode="rt", encoding="utf-8") as words_file:
+	for line in words_file:
+		if line != u'\n':
+			words.append(line.rstrip('\n'))
