2ccebaa9ef7ad77c6999685802271cdb742885ee
Raznor <62889081+Raznor@users.noreply.github.com>
2020-06-09 16:17:50 +0200

    Initial commit

---
+++ wiktionary-scraper.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+"""
+Wiktionary Scraper for FDTT
+---------------------------
+This is Python 2.7 program using lxml and requests libraries to scrape
+declension table data from Wiktionary.org.
+"""
+
+# Max line length w/ --- w/o flowing text blocks (PEP 8 style guide): 72 --- 79
