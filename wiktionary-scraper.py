fafe95b9c91e452e9032ffa85ce5a41e6f4df6b9
Raznor87 <32267708+Raznor87@users.noreply.github.com>
2020-06-21 21:11:10 +0300

    exception handling, comments updated, removed testing statements

--- wiktionary-scraper.py
+++ wiktionary-scraper.py
@@ -27,53 +27,58 @@ def scrape_declension_table(word, wtype="Noun", language="Finnish"):
 	'<accusative-nominative singular>', '<accusative plural>', 
 	'<accusative-genitive singular>',
 	'<genitive singular>', '<genitive plural>',
-	'<partitive singular>', '<partitive plural>', <etc.>
+	'<partitive singular>', '<partitive plural>', etc.
+	In case there are multiple correct forms at a certain case,
+	a tuple of strings is used.
 	"""
+
+	# return empty list if wtype is invalid
+	valid_wtypes = {
+	"Noun", "noun", "Adjective", "adjective", "Numeral", "numeral", "Pronoun", "pronoun"
+	}
+	if wtype not in valid_wtypes:
+		print("type '" + wtype + "' is not a valid word type")
+		return []
+	
 	response = requests.get(URL_PREFIX + word)
 	tree = html.fromstring(response.content)
 
-	lang_on_page = tree.xpath('//h2//*[text()="' + language + '"]')    # select all h2 elements that have text 'language' downstream
+	# select all h2 elements that have text 'language' downstream
+	lang_on_page = tree.xpath('//h2//*[text()="' + language + '"]')
 	# return empty list if the page doesn't have the required language
 	if lang_on_page == []:
+		print("scraper couldn't find language '" + language + "' at location " + URL_PREFIX + word)
 		return []
-	# print("lang_on_page[0]:", lang_on_page[0], "text:", lang_on_page[0].xpath('.//text()'))
 	
 	lang_h2 = lang_on_page[0].xpath('./..')
-	# print("lang_h2:", lang_h2, "text:", lang_h2[0].xpath('.//text()'))
 	
-	next_lang_h2 = lang_on_page[0].xpath('./ancestor::h2/following-sibling::h2')    # select the next h2 sibling, probably the next language's section heading
+	# select the next h2 sibling, probably the next language's section heading
 	next_lang_h2 = lang_h2[0].xpath('./following-sibling::h2[1]')
 	if next_lang_h2 == []:
 		next_lang_text = ""
 	else: 
 		next_lang_text = next_lang_h2[0].xpath('.//text()')
-	# print("next_lang_h2:", next_lang_h2, "next_lang_text:", next_lang_text)
 
-	
 	curr_elem = lang_h2
 	while True:
 		next_elem = curr_elem[0].xpath('./following-sibling::*[1]')
-		if next_elem == []:    # if there are no more siblings, end search
+		# if there are no more siblings, end search
+		if next_elem == []:
 			break
-		# print "next_elem[0]:", next_elem[0]
 		if next_elem[0].tag in ["h3", "h4", "h5", "h6"]:
-			if wtype in next_elem[0].xpath('.//text()'):    # if the desired word class is found, end search
-				# print "Found " + wtype + " in sibling " + next_elem[0].tag + "!"
+			# if the desired word class is found, end search
+			if wtype in next_elem[0].xpath('.//text()'):
 				wtype_elem = next_elem[0]
 				break
-			# else:
-			# 	print next_elem[0].xpath('.//text()')
-			# 	print "Haven't found " + wtype + " in sibling " + next_elem[0].tag + ".."
-		elif next_elem[0].tag == "h2":    # if search finds next h2 element, probably the next language's section, end search
-			# print "No sibling element with " + wtype + " type. Searched " + str(counter) + " elements."
-			break
+		# if search finds next h2 element, probably the next language's section, return empty list
+		elif next_elem[0].tag == "h2":
+			print("scraper couldn't find type '" + wtype + "' in language '" + language + "'")
+			return []
 		curr_elem = next_elem
-
-	# print("wtype_elems:", wtype_elem, "text:", wtype_elem[0].xpath('.//text()'))
 
 	table = wtype_elem[0].xpath('./following::table[1]')
 	# print("table:", table)
-	if wtype == "Noun":
+	if wtype in {"Noun", "noun"}:
 		raw_table_data = table[0].xpath('./tbody/tr[@class="vsHide"]/td//text()')
 		# print("raw_table_data:", raw_table_data)
 		table_data = []
@@ -87,9 +92,12 @@ def scrape_declension_table(word, wtype="Noun", language="Finnish"):
 		# print("table_data:", table_data)
 		return table_data
 
-	elif wtype == "Pronoun":
+	elif wtype in {"Pronoun", "pronoun"}:
 		return
-
+	elif wtype in {"Adjective", "adjecive"}:
+		return
+	elif wtype in {"Numeral", "numeral"}:
+		return
 
 
 words = []
